6c3e7764b41716b693918c7efa77081b138d93c0
treshshina-git <treshshina@gmail.com>
2026-06-24 22:50:10 +0200

    3

--- bot.py
+++ bot.py
@@ -1,10 +1,51 @@
 import os
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
 from telegram.ext import Application, InlineQueryHandler, ContextTypes
+from fastapi import FastAPI, Request
 
 BOT_TOKEN=os.getenv("BOT_TOKEN")
-APP_URL=os.getenv("APP_URL")
+WEBHOOK_URL=os.getenv("APP_URL")
 PORT=int(os.getenv("PORT"))
+WEBHOOK_SECRET="secret"
+
+app = FastAPI()
+
+tg_app = setup_app()
+
+
+@app.on_event("startup")
+async def startup():
+
+    validate_config()
+
+    await tg_app.initialize()
+
+    await tg_app.bot.set_webhook(
+        url=WEBHOOK_URL,
+        secret_token=WEBHOOK_SECRET
+    )
+
+
+@app.post("/webhook")
+async def webhook(request: Request):
+
+    data = await request.json()
+
+    update = Update.de_json(data, tg_app.bot)
+
+    await tg_app.process_update(update)
+
+    return {"ok": True}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+
+
+
 
 async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query=update.inline_query.query.strip()
@@ -31,6 +72,6 @@ if __name__=="__main__":
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
-        webhook_url=f"{APP_URL}/{BOT_TOKEN}",
+        webhook_url=f"{APP_URL}",
         secret_token=BOT_TOKEN
     )
